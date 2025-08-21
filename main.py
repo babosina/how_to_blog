b5dcb94feb1f6c176bbc51aa7911d50989abd515
astankevich <pythonqa5@gmail.com>
2025-08-21 21:05:09 +0400

    t-9: add db connection; remove static posts url request

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import os
 from flask import Flask, render_template, request
 from datetime import datetime
 from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from datetime import datetime, date
+from post_form import PostForm
 
 load_dotenv()
 
-URL = "https://api.npoint.io/405401fd224c5d58a066"
-
-posts = requests.get(URL).json()
-current_year = datetime.now().year
 MY_EMAIL = os.getenv("MY_EMAIL")
 MY_PASSWORD = os.getenv("MY_PASSWORD")
 SMTP_SERVER = os.getenv("SMTP_SERVER")
@@ -19,12 +22,38 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", posts=posts,
-                           current_year=current_year)
+    posts = db.session.execute(db.Select(BlogPost)).scalars().all()
+    return render_template("index.html", posts=posts)
 
 
 @app.route("/about")
@@ -52,12 +81,8 @@ Message from {request.form.get('email')}: {request.form.get('message')}
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
-    requested_post = None
-    for post in posts:
-        if post.get('id') == post_id:
-            requested_post = post
-    return render_template("post.html", post=requested_post,
-                           current_year=current_year)
+    post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
