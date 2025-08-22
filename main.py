4338aeb1b9e544da48c8bede43013b9eedb39b2c
astankevich <pythonqa5@gmail.com>
2025-08-22 16:08:48 +0400

    t-11: move db init to a separate class

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from datetime import datetime
 from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
 from datetime import datetime, date
 from post_form import PostForm
+from database import db, BlogPost
 
 load_dotenv()
 
@@ -22,30 +20,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 Bootstrap5(app)
 
-
-# CREATE DATABASE
-class Base(DeclarativeBase):
-    pass
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
-
-# CONFIGURE TABLE
-class BlogPost(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-    date: Mapped[str] = mapped_column(String(250), nullable=False)
-    body: Mapped[str] = mapped_column(Text, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
-
-
 with app.app_context():
     db.create_all()
 
